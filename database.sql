e11f4d4127e4d0b61f7fb3d2dd477c2d35da8951
Rifan <tubagusrifan@gmail.com>
2024-10-04 20:11:30 +0700

    database sql query

---
+++ database.sql
@@ -0,0 +1,27 @@
+-- 1. Membuat Database
+CREATE DATABASE colors_db;
+
+-- 2. Menggunakan Database yang baru dibuat
+\c colors_db;
+
+-- 3. Membuat Tabel "v_color"
+CREATE TABLE v_color (
+    id SERIAL PRIMARY KEY,
+    c_name VARCHAR(50) NOT NULL,
+    c_hex VARCHAR(7) NOT NULL,
+    c_rgb VARCHAR(20) NOT NULL
+);
+
+-- 4. Memasukkan Data Warna
+INSERT INTO v_color (c_name, c_hex, c_rgb)
+VALUES 
+('Red', '#FF0000', 'rgb(255, 0, 0)'),
+('Green', '#008000', 'rgb(0, 128, 0)'),
+('Blue', '#0000FF', 'rgb(0, 0, 255)'),
+('Yellow', '#FFFF00', 'rgb(255, 255, 0)'),
+('Cyan', '#00FFFF', 'rgb(0, 255, 255)'),
+('Magenta', '#FF00FF', 'rgb(255, 0, 255)'),
+('Black', '#000000', 'rgb(0, 0, 0)'),
+('White', '#FFFFFF', 'rgb(255, 255, 255)'),
+('Orange', '#FFA500', 'rgb(255, 165, 0)'),
+('Purple', '#800080', 'rgb(128, 0, 128)');
